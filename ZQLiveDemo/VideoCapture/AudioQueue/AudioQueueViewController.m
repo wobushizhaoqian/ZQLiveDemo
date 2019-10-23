ae227caea2f7a4e89cae87b139ad746bbc96da32
zhaoqian <freepeoplezhao@163.com>
2019-10-23 15:08:19 +0800

    AudioQueue

--- ZQLiveDemo/VideoCapture/AudioQueue/AudioQueueViewController.m
+++ ZQLiveDemo/VideoCapture/AudioQueue/AudioQueueViewController.m
@@ -9,6 +9,8 @@
 #import "AudioQueueViewController.h"
 
 @interface AudioQueueViewController ()
+@property (nonatomic, strong)UIButton *startButton;
+@property (nonatomic, strong)UIButton *stopButton;
 
 @end
 
@@ -16,17 +18,45 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self.view addSubview:self.startButton];
+    [self.view addSubview:self.stopButton];
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark -- evevts
+- (void)respondsToStartButton:(UIButton *)sender{
 }
-*/
+- (void)respondsToStopButton:(UIButton *)sender{
+}
+
+#pragma mark -- system methods
+- (void)viewWillLayoutSubviews{
+    [super viewWillLayoutSubviews];
+    self.startButton.frame = CGRectMake(0, 0, 150, 30);
+    self.startButton.center = CGPointMake(self.view.bounds.size.width/4, self.view.bounds.size.height - 30);
+    
+    self.stopButton.frame = CGRectMake(0, 0, 150, 30);
+    self.stopButton.center = CGPointMake(self.view.bounds.size.width *3/4, self.view.bounds.size.height - 30);
+}
+#pragma mark -- lazy loading
+- (UIButton *)startButton{
+    if (!_startButton) {
+        _startButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_startButton setTitle:@"开始采集音频" forState:UIControlStateNormal];
+        [_startButton addTarget:self action:@selector(respondsToStartButton:) forControlEvents:UIControlEventTouchUpInside];
+        _startButton.backgroundColor = [UIColor randomColor];
+    }
+    return _startButton;
+}
+
+- (UIButton *)stopButton{
+    if (!_stopButton) {
+        _stopButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_stopButton setTitle:@"停止采集音频" forState:UIControlStateNormal];
+        [_stopButton addTarget:self action:@selector(respondsToStopButton:) forControlEvents:UIControlEventTouchUpInside];
+        _stopButton.backgroundColor = [UIColor randomColor];
+    }
+    return _stopButton;
+}
 
 @end
